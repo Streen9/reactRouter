0f9d9e918cf52861ac511d7e61d7c7cc94329714
Streen9 <gkvp70@gmail.com>
2024-03-09 00:04:41 +0530

    commented the react-query

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
 export default function HomePage() {
-  const { data } = useQuery({
-    queryKey: ["todo"],
-    queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
-        res.json()
-      ),
-  });
+  // const { data } = useQuery({
+  //   queryKey: ["todo"],
+  //   queryFn: () =>
+  //     fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+  //       res.json()
+  //     ),
+  // });
 
-  console.log(data);
 
   return <div>Home Page</div>;
 }
